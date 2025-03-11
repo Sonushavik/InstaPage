a3091da6532d8e42e4b48999ba98b5ce9958b2c0
Sonushavik <sonuk06212@gmail.com>
2025-03-11 18:55:42 +0530

    created user insta profile page using ejs

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const users = require('./data.json');
+const path = require('path')
+const app = express();
+const PORT = 8080
+
+app.use(express.static(path.join(__dirname, "./public/images")));
+app.use(express.static(path.join(__dirname,("./public"))))
+
+
+app.set('view engine', 'ejs');
+
+app.get("/", (req,res) => {
+        res.render("home.ejs")
+})
+
+app.get("/ig/:userid", (req, res) => {
+        let {userid} = req.params;
+        const user = users.users.find(user => user.userid === userid);
+        const postLength = user.posts.length;
+        res.render("userProfile.ejs", { user, postLength });
+    });
+    
+app.listen(PORT, () => {
+        console.log(`App is listening on localhost:${PORT}`)
+})
